f14494cf52d4aa4da540c96a994e994ab3354538
Amartya Gunjan <53094015+agile-flight@users.noreply.github.com>
2024-06-22 12:00:38 -0500

    Co-authored-by: Kwaraz <Kwaraz@users.noreply.github.com> Co-authored-by: Mingche <MingcheL1@users.noreply.github.com>

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const { express, bodyParser, helmet, morgan, cors} = require('./middlewares');
+
+const PORT = 4000;
+const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:4000",
+        credentials: true
+    })
+)
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.listen(PORT, console.log(`API is listening on port ${PORT}`));
+
--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link"
 import { FC } from "react"
 
 
 export const Navbar:FC=()=>{
     return(
-        <div>
-            
+        <div className="bg-black py-2 w-full h-auto jetbrains-mono">
+            <div className="grid grid-cols-[1fr_max-content_1fr]  place-items-center px-4">
+                <div className="">
+                    <Link href="/">Home</Link>
+                </div>
+                <div className="grid grid-flow-col gap-12 text-2xl">
+                    <Link href="" className="text-white">About</Link>
+                    <Link href="" className="text-white">Notes</Link>
+                    <Link href="" className="text-white">Sharing</Link>
+                    
+                </div>
+            </div>
         </div>
     )
 }
