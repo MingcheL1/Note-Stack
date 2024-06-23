8bcfadfe72d4588cbe83b17c8120bcb7c04ef53a
BrotherBei <132795672+BrotherBei@users.noreply.github.com>
2024-06-22 22:06:50 -0500

    changed home page

--- frontend/app/ai/page.tsx
+++ frontend/app/ai/page.tsx
@@ -27,9 +27,9 @@ const Page = () => {
         <Navbar/>
         <h2 className="text-center font-bold text-4xl mt-10 ">Select a notetaking method:</h2>
         <select className="rounded-lg text-white text-2xl mt-10 h-auto bg-gradient-to-r from-violet-600 to-indigo-800"value={selectedComponent} onChange={handleSelectChange}>
-          <option value="">Select...</option>
-          <option value="text">Text to Notes</option>
-          <option value="audio">Audio to Notes</option>
+          <option className="bg-black opacity-50" value="">Select...</option>
+          <option className="bg-black opacity-50" value="text">Text to Notes</option>
+          <option className="bg-black opacity-50" value="audio">Audio to Notes</option>
         </select>
         
         <div style={{ marginTop: '20px' }}>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -41,21 +41,13 @@ export default function Home() {
       
   
       {/* <h1 className="text-white text-8xl mt-52 ml-20 font-bold">Note Stack</h1> */}
-      <div className="w-auto p-10 bg-black">
-        <button className="mt-10 ml-12 w-52 rounded-full bg-gradient-to-r from-violet-600 to-indigo-800 text-white text-2xl container_anim">
+      <div className=" w-auto p-10 bg-black flex flex-row items-center justify-center space-x-8">
+        <button className=" w-52 rounded-full bg-gradient-to-r from-violet-600 to-indigo-800 text-white text-2xl container_anim">
           <p className="p-4">Register</p>
         </button>
-        <button className="mt-10 ml-12 w-52 rounded-full bg-gradient-to-r from-blue-600 to-pink-600 text-white text-2xl container_anim">
+        <button className=" w-52 rounded-full bg-gradient-to-r from-blue-600 to-pink-600 text-white text-2xl container_anim">
           <p className="p-4">View More</p>
         </button>
-      </div>
-      <div>
-        <video
-          className="absolute top-62 left-0 w-full h-screen object-cover"
-          src="https://eubzkoywhckxuyrjsrje.supabase.co/storage/v1/object/public/website/line-waves.webm?t=2024-03-19T22%3A09%3A07.266Z"
-          loop
-          playsInline
-        ></video>
       </div>
     </div>
     </>
